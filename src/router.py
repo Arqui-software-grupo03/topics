b2e3689bd6b7469b50a4a8775d5d6e16b0100df9
cjsalame <cjsalame@uc.cl>
2018-11-01 13:51:25 -0300

    subscribers working

--- src/router.py
+++ src/router.py
@@ -1,7 +1,9 @@
-from topics.views import TopicViewSet, PostIdViewSet
+from topics.views import TopicViewSet, PostIdViewSet, SubscriberViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('topics', TopicViewSet)
 router.register('post_ids', PostIdViewSet)
+router.register('subscribers', SubscriberViewSet)
 router.register(r'topics/(?P<topic>[^/.]+)/post_ids', PostIdViewSet)
+router.register(r'topics/(?P<topic>[^/.]+)/subscribers', SubscriberViewSet)
--- topics/serializers.py
+++ topics/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Topic, PostId
+from .models import Topic, PostId, Subscriber
 
 class PostIdSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -11,9 +11,20 @@ class PostIdSerializer(serializers.HyperlinkedModelSerializer):
                                         topic=validated_data['topic'])
         return post_id
 
+class SubscriberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Subscriber
+        fields = ['user_id', 'topic']
+
+    def create(self, validated_data):
+        subscriber = Subscriber.objects.create(user_id=validated_data['user_id'],
+                                        topic=validated_data['topic'])
+        return subscriber
+
 class TopicSerializer(serializers.HyperlinkedModelSerializer):
     post_ids = PostIdSerializer(required=False, many = True)
+    subscribers = SubscriberSerializer(required=False, many=True)
 
     class Meta:
         model = Topic
-        fields = ['id', 'title', 'description', 'post_ids']
+        fields = ['id', 'title', 'description', 'post_ids', 'subscribers']
